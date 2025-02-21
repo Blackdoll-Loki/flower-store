4ec22dee178eca6d85b971242d414f31dcb59785
Blackdoll-Loki <helpmeloki7@gmail.com>
2025-02-21 13:34:00 +0200

    created service and controller

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export interface FlowerObj{
+  name: string,
+  color: string,
+  price: number
+}
+
 @Injectable()
-export class FlowersService {}
+export class FlowersService {
+  findAll(): FlowerObj[]{
+    return [
+      {
+        name: 'Rose',
+        color: 'Red',
+        price: 5,
+      },
+      {
+        name: 'Lily',
+        color: 'White',
+        price: 6,
+      },
+      {
+        name: 'Tulip',
+        color: 'Yellow',
+        price: 7,
+      }
+    ];
+  }
+}
